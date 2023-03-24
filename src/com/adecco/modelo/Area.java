8b7dabf61643652db4c0ce629dd27bef48afc939
vvalverde76 <vvalverde76@gmail.com>
2023-03-24 12:07:59 +0100

    area circulo

--- src/com/adecco/modelo/Area.java
+++ src/com/adecco/modelo/Area.java
@@ -1,2 +1,11 @@
-package com.adecco.modelo;public class Area {
+package com.adecco.modelo;
+
+public class Area {
+
+    public float areaCirculo( float radio){
+        return   radio*3.14159f*radio;
+    }
+    public float areaRectangulo (float base,float altura){
+        return base*altura;
+    }
 }
--- src/com/adecco/presentacion/Main.java
+++ src/com/adecco/presentacion/Main.java
@@ -1,5 +1,16 @@
+package com.adecco.presentacion;
+
+import com.adecco.modelo.Area;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Area a1 = new Area();
+        System.out.println("El area del cuadrado es = "+
+                a1.areaRectangulo(4f,5f));
+
+        Area a2 = new Area();
+        System.out.println( "El area del circulo es = "+
+                a2.areaCirculo(6.77f));
     }
 }
